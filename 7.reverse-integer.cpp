44d89a2da6d833dd2663f04d15528ef1fec19a0a
Sarim Ahmed <C41f0N@protonmail.com>
2024-12-18 21:01:45 +0500

    solved

---
+++ 7.reverse-integer.cpp
@@ -0,0 +1,139 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+/*
+ * @lc app=leetcode id=7 lang=cpp
+ *
+ * [7] Reverse Integer
+ */
+
+// @lc code=start
+class Solution
+{
+public:
+    long int reverse(long int x)
+    {
+        cout << x << endl;
+        long int xx = x;
+
+        bool sign = x < 0;
+
+        string max = "2147483647"; // 2^31 - 1 = 2147483647
+        string min = "2147483648"; // - 2^31 = -2147483648
+        string s = "";
+
+        if (x == -2147483648)
+        {
+            return 0;
+        }
+
+        // cout << xx << endl;
+        if (xx == 0)
+        {
+            s = "0";
+        }
+        else
+        {
+
+            while (xx)
+            {
+                if (sign)
+                {
+                    s += (xx % 10) * -1 + '0';
+                }
+                else
+                {
+                    s += (xx % 10) + '0';
+                }
+                xx /= 10;
+            }
+        }
+
+        if (sign)
+        {
+            // The number should be lesser than or equal to 4294967296
+            if (s.length() == 10)
+            {
+                // Compare
+                for (int i = 0; i < 10; i++)
+                {
+                    if (s[i] < min[i])
+                    {
+                        break;
+                    }
+                    else if (s[i] > min[i])
+                    {
+                        return 0;
+                    }
+                }
+            }
+            else if (s.length() > 10)
+            {
+                return 0;
+            }
+        }
+        else
+        {
+            // The number should be lesser than or equal to 4294967295
+            if (s.length() == 10)
+            {
+                // Compare
+                for (int i = 0; i < 10; i++)
+                {
+                    if (s[i] < max[i])
+                    {
+                        break;
+                    }
+                    else if (s[i] > max[i])
+                    {
+                        return 0;
+                    }
+                }
+            }
+            else if (s.length() > 10)
+            {
+                return 0;
+            }
+        }
+
+        cout << s << endl;
+
+        long int res = 0;
+        // Converting string back to int
+        for (int i = 0; i < s.length(); i++)
+        {
+            if (sign)
+            {
+                long tenpow = 1;
+                long charr = s[i];
+                for (int j = 0; j < s.length() - 1 - i; j++)
+                {
+                    tenpow *= 10;
+                }
+                res -= (charr - '0') * tenpow;
+            }
+            else
+            {
+                long tenpow = 1;
+                long charr = s[i];
+                for (int j = 0; j < s.length() - 1 - i; j++)
+                {
+                    tenpow *= 10;
+                }
+                // cout << tenpow << endl;
+
+                res += (charr - '0') * tenpow;
+            }
+        }
+
+        return res;
+    }
+};
+// @lc code=end
+
+int main()
+{
+    Solution s;
+
+    cout << s.reverse(4294967224) << endl;
+}
